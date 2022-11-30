b8dc5a371e8c1ddc45f7b32115753d8ba4c6003b
linas92 <undeadarmyleader@gmail.com>
2022-11-30 10:30:07 +0200

    testing if everything is working

--- dnd/dnd/settings.py
+++ dnd/dnd/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # "monsters",
+    "dnd_monsters",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
