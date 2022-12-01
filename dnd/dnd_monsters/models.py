ace908d4264a8959d3a6b7af89e57240dd870a72
linas92 <undeadarmyleader@gmail.com>
2022-12-01 09:38:41 +0200

    process of making models, focused,need a walk,food

--- dnd/dnd_monsters/models.py
+++ dnd/dnd_monsters/models.py
@@ -4,19 +4,19 @@ from datetime import date
 
 
 
-class Monster(models.Model):#kaip book
+class Monster(models.Model):
     name = models.CharField(_("name"), max_length=50)
     size = models.ForeignKey(Size, on_delete=models.SET_NULL, null=True, blank=True, related_name="monsters", )
 
 
-class Type(models.Model):#kaip genre
+class Type(models.Model):
     type = models.CharField(_("type"), max_length=200, help_text=_("Enter the type of the beast/monster"))
 
     def __str__(self) -> str:
         return self.type
 
 
-class Size(models.Model):#darau kaip authors
+class Size(models.Model):
     tiny = models.CharField(_())
     small = 
     medium = 
